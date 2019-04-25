6571680b3c754e1c1b80e335ac968cec06c64295
Peter Morgan <codeup@1080peter.com>
2019-04-24 21:06:42 -0600

    added more routes

--- routes/index.js
+++ routes/index.js
@@ -10,20 +10,32 @@ router.get('/error', (req, res) => {
   res.render('error');  
 });
 
+router.get('/register', (req, res) => {
+  res.render('register');  
+});
+
+router.post('/register', (req, res) => {
+  res.render('register');  
+});
+
 router.get('/login', (req, res) => {
   res.render('login');  
 });
 
-router.get('/new', (req, res) => {
-  res.render('new');  
+router.post('/login', (req, res) => {
+  res.render('login');  
 });
 
 router.get('/profile', (req, res) => {
   res.render('profile');  
 });
 
-router.get('/register', (req, res) => {
-  res.render('register');  
+router.get('/new', (req, res) => {
+  res.render('new');  
+});
+
+router.post('/new', (req, res) => {
+  res.render('new');  
 });
 
 router.get('/logout', (req, res) => {
