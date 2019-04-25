b74f8e7348a024ccffc5b83d103e74c9fb794624
Peter Morgan <codeup@1080peter.com>
2019-04-25 14:20:44 -0600

    register post

--- routes/index.js
+++ routes/index.js
@@ -4,40 +4,57 @@ var User = require('../models/user');
 /* middleware */
 
 router.get('/', (req, res, next) => {
-  return res.render('index', {title: 'Welcome'});  
+  return res.render('index', { title: 'Welcome' });
   // if logged in, go to home.pug ---- res.render('home'); 
 });
 
 router.get('/register', (req, res, next) => {
-  return res.render('register', {title: 'Register'});  
+  return res.render('register', { title: 'Register' });
 });
 
 router.post('/register', (req, res, next) => {
-  res.render('register');  
+  if (req.body.usernameInput && req.body.passwordInput) {
+    var userData = {
+      username: req.body.usernameInput,
+      password: req.body.passwordInput
+    };
+
+    User.create(userData, function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  } else {
+    var err = new Error('All fields are required to register.');
+    err.status = 400;
+    return next(err);
+  }
 });
 
 router.get('/login', (req, res, next) => {
-  return res.render('login', {title: 'Log In'});  
+  return res.render('login', { title: 'Log In' });
 });
 
 router.post('/login', (req, res, next) => {
-  res.render('login');  
+  res.render('login');
 });
 
 router.get('/profile', (req, res, next) => {
-  return res.render('profile', {title: 'Your Profile'});  
+  return res.render('profile', { title: 'Your Profile' });
 });
 
 router.get('/new', (req, res, next) => {
-  return res.render('new', {title: 'New Message'});  
+  return res.render('new', { title: 'New Message' });
 });
 
 router.post('/new', (req, res, next) => {
-  res.render('new');  
+  res.render('new');
 });
 
 router.get('/logout', (req, res, next) => {
-  res.redirect('/');  
+  res.redirect('/');
 });
 
 module.exports = router;
