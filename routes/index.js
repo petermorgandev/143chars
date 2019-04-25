0889b96d491c82450fe7d935da35f148e443cd39
Peter Morgan <codeup@1080peter.com>
2019-04-24 20:54:02 -0600

    defined initial routes

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,24 @@ router.get('/', (req, res) => {
   res.render('index');  
 });
 
+router.get('/error', (req, res) => {
+  res.render('error');  
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');  
+});
+
+router.get('/new', (req, res) => {
+  res.render('new');  
+});
+
+router.get('/profile', (req, res) => {
+  res.render('profile');  
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');  
+});
 
 module.exports = router;
