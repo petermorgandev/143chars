b56bf8e49d3495138b6e899bf2a8349cd3d9df4a
Peter Morgan <codeup@1080peter.com>
2019-04-25 14:13:10 -0600

    added user model to routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-/* user schema from models */
+var User = require('../models/user');
 /* middleware */
 
 router.get('/', (req, res, next) => {
