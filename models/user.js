1f3279cfb798f7a0da3c7eb772fc9e98c92aa5b6
Peter Morgan <codeup@1080peter.com>
2019-04-25 12:51:59 -0600

    user model

--- models/user.js
+++ models/user.js
@@ -13,9 +13,35 @@ var UserSchema = new mongoose.Schema({
   }
 });
 
-/* authenticate input against database documents */
+UserSchema.statics.authenticate = function (username, password, callback) {
+  User.findOne({ username: username })
+    .exec(function (error, user) {
+      if (error) { return callback(error); }
+      else if (!user) {
+        var err = new Error('User not found');
+        err.status = 401;
+        return callback;
+      }
+      bcrypt.compare(password, user.password, function (error, result) {
+        if (result === true) {
+          return callback(null, user);
+        } else {
+          return callback();
+        }
+      });
+    });
+};
 
-/* hash the password */
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  });
+});
 
 var User = mongoose.model('User', UserSchema);
 module.exports = User;
