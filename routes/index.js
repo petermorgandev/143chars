68d2f89c833a424523587fb1c2d3da9a22e57a35
Peter Morgan <codeup@1080peter.com>
2019-04-24 21:04:42 -0600

    additional set up files

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.render('index');  
+  // if logged in, go to home.pug
 });
 
 router.get('/error', (req, res) => {
@@ -23,6 +24,10 @@ router.get('/profile', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register');  
+});
+
+router.get('/logout', (req, res) => {
+  res.redirect('/');  
 });
 
 module.exports = router;
