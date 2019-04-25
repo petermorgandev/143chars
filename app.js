03afd17a3089162deee54c2d5d17ea98a3aa547b
Peter Morgan <codeup@1080peter.com>
2019-04-25 14:51:21 -0600

    session tracking

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-/* mongostore */
+const mongoStore = require('connect-mongo')(session);
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/143chars', { useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/143chars', { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-/* session tracking */
+app.use(session({
+  secret: 'this is a super secret phrase',
+  resave: true,
+  saveUninitialized: false,
+  store: new mongoStore({ mongooseConnection: db })
+}));
 
 /* pass user id into the templates */
 
@@ -24,14 +29,14 @@ app.use(routes);
 app.use(express.static('public'));
 
 // deal with 404 errors and pass along to generic error handler
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
   var err = new Error('File Not Found');
   err.status = 404;
   next(err);
 });
 
 // Generic error handler
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
