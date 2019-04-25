bc7bb33067b5ebfe0bbc978bf9f9b055b46e16e1
Peter Morgan <codeup@1080peter.com>
2019-04-25 14:44:33 -0600

    more route works

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ router.get('/', middle.homepage, (req, res, next) => {
   return res.render('index', { title: 'Welcome' });
 });
 
-router.get('/register', (req, res, next) => {
+router.get('/register', middle.loggedOut, (req, res, next) => {
   return res.render('register', { title: 'Register' });
 });
 
-router.post('/register', middle.loggedOut, (req, res, next) => {
+router.post('/register', (req, res, next) => {
   if (req.body.usernameInput && req.body.passwordInput) {
     var userData = {
       username: req.body.usernameInput,
@@ -37,11 +37,38 @@ router.get('/login', middle.loggedOut, (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
-  res.render('login');
+  if (req.body.usernameInput && req.body.passwordInput) {
+    User.authenticate(req.body.usernameInput, req.body.passwordInput, function (error, user) {
+      if (error || !user) {
+        var err = new Error('Wrong username or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/');
+      }
+    });
+  } else {
+    var err = new Error('Username and password are required to log in.');
+    err.status = 401;
+    return next(err);
+  }
 });
 
 router.get('/profile', middle.requiresLogin, (req, res, next) => {
-  return res.render('profile', { title: 'Your Profile' });
+  if (!req.session.userId) {
+    var err = new Error('You are not authorized to view this page.');
+    err.status = 403;
+    return next(err);
+  }
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.render('profile', { title: 'Your Profile' /* i think the messages go in here */ });
+      }
+    });
 });
 
 router.get('/new', middle.requiresLogin, (req, res, next) => {
@@ -53,7 +80,15 @@ router.post('/new', (req, res, next) => {
 });
 
 router.get('/logout', (req, res, next) => {
-  res.redirect('/');
+  if (req.session) {
+    req.session.destroy(function (err) {
+      if (err) {
+        return next(err);
+      } else {
+        res.redirect('/');
+      }
+    });
+  }
 });
 
 module.exports = router;
