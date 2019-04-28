ffac473dbb9e5861d70482696d9d3b95bb0c86ea
Peter Morgan <codeup@1080peter.com>
2019-04-27 19:20:36 -0600

    bootstrapped more view

--- middleware/index.js
+++ middleware/index.js
@@ -1,12 +1,13 @@
 function loggedOut(req, res, next) {
-  if (req.session & req.session.userID) {
+  if (req.session && req.session.userId) {
     return res.redirect('/');
+  } else {
+    return next();
   }
-  return next();
 };
 
 function requiresLogin(req, res, next) {
-  if (req.session && req.session.userID) {
+  if (req.session && req.session.userId) {
     return next();
   } else {
     var err = new Error('You must be logged into view this page.');
