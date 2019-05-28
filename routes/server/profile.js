222790b0829d09618fa758471ddd4ed01634a76d
Petermorgangh <peter@1080peter.com>
2019-05-27 22:28:19 -0600

    Destructured middleware require in /routes/server/profile.js

--- routes/server/profile.js
+++ routes/server/profile.js
@@ -2,13 +2,13 @@ const express = require("express"),
   router = express.Router([{ mergeParams: true }]),
   User = require("../../models/user"),
   Message = require("../../models/messages"),
-  middle = require("../../middleware");
+  { requiresLogin } = require("../../middleware");
 
-router.get("/", middle.requiresLogin, (req, res, next) => {
+router.get("/", requiresLogin, (req, res, next) => {
   return res.redirect("/profile/" + req.session.userId);
 });
 
-router.get("/:userId", middle.requiresLogin, async function(req, res, next) {
+router.get("/:userId", requiresLogin, async function(req, res, next) {
   const user = await User.findOne({ _id: { $in: req.params.userId } });
 
   await Message.find({ user: { $in: req.params.userId } })
