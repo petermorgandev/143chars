d347960606ec5742a8182ae69a6343696e82b8ba
Petermorgangh <peter@1080peter.com>
2019-05-28 12:48:58 -0600

    Refactor server /delete routes

--- routes/server/delete.js
+++ routes/server/delete.js
@@ -4,11 +4,7 @@ const express = require("express"),
   Message = require("../../models/messages"),
   { requiresLogin, isCurrentUser } = require("../../middleware");
 
-router.get("/message/:messageId", requiresLogin, async function(
-  req,
-  res,
-  next
-) {
+router.get("/message/:messageId", requiresLogin, async (req, res, next) => {
   const userId = await Message.findById({ _id: req.params.messageId });
 
   if (!req.session.userId || userId.user != req.session.userId) {
@@ -17,47 +13,36 @@ router.get("/message/:messageId", requiresLogin, async function(
     return next(err);
   }
 
-  await Message.deleteOne({ _id: { $in: req.params.messageId } }).exec(function(
-    error
-  ) {
-    if (error) {
-      return next(error);
-    } else {
-      return res.redirect("/profile");
-    }
-  });
+  await Message.deleteOne({ _id: { $in: req.params.messageId } })
+    .exec()
+    .then(() => res.redirect("/profile"))
+    .catch(error => next(error));
 });
 
-router.get("/messages/:userId", requiresLogin, isCurrentUser, function(
-  req,
-  res,
-  next
-) {
-  Message.deleteMany({ user: { $in: req.params.userId } }).exec(function(
-    error
-  ) {
-    if (error) {
-      return next(error);
-    } else {
-      return res.redirect("/profile");
-    }
-  });
-});
+router.get(
+  "/messages/:userId",
+  requiresLogin,
+  isCurrentUser,
+  (req, res, next) => {
+    Message.deleteMany({ user: { $in: req.params.userId } })
+      .exec()
+      .then(() => res.redirect("/profile"))
+      .catch(error => next(error));
+  }
+);
 
-router.get("/user/:userId", requiresLogin, isCurrentUser, async function(
-  req,
-  res,
-  next
-) {
-  await Message.deleteMany({ user: { $in: req.session.userId } });
+router.get(
+  "/user/:userId",
+  requiresLogin,
+  isCurrentUser,
+  async (req, res, next) => {
+    await Message.deleteMany({ user: { $in: req.session.userId } });
 
-  await User.deleteOne({ _id: req.session.userId }).exec(function(error) {
-    if (error) {
-      return next(error);
-    } else {
-      return res.redirect("/logout");
-    }
-  });
-});
+    await User.deleteOne({ _id: req.session.userId })
+      .exec()
+      .then(() => res.redirect("/logout"))
+      .catch(error => next(error));
+  }
+);
 
 module.exports = router;
