3383aae383f56e9a38eb1fd776a185b0fa640181
Petermorgangh <peter@1080peter.com>
2019-05-27 22:10:12 -0600

    Refactor routes/server/profile.js
    Removed duplicative check for useriD in session request.

--- routes/server/profile.js
+++ routes/server/profile.js
@@ -5,22 +5,10 @@ const express = require("express"),
   middle = require("../../middleware");
 
 router.get("/", middle.requiresLogin, (req, res, next) => {
-  if (!req.session.userId) {
-    var err = new Error("You are not authorized to view this page.");
-    err.status = 403;
-    return next(err);
-  }
-
   return res.redirect("/profile/" + req.session.userId);
 });
 
 router.get("/:userId", middle.requiresLogin, async function(req, res, next) {
-  if (!req.session.userId) {
-    var err = new Error("You are not authorized to view this page.");
-    err.status = 403;
-    return next(err);
-  }
-
   const user = await User.findOne({ _id: { $in: req.params.userId } });
 
   await Message.find({ user: { $in: req.params.userId } })
