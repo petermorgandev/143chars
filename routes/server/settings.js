e3ce83b0fb6053fa984b7350bd47b19cb5b34fa7
Petermorgangh <peter@1080peter.com>
2019-05-28 13:07:40 -0600

    Refactor routes/server/settings.js

--- routes/server/settings.js
+++ routes/server/settings.js
@@ -4,29 +4,27 @@ const express = require("express"),
   { requiresLogin } = require("../../middleware");
 
 router.get("/", requiresLogin, (req, res, next) => {
-  User.find({ _id: { $in: req.session.userId } }).exec(function(error, user) {
-    if (error) {
-      return next(error);
-    } else {
-      return res.render("settings", {
+  User.find({ _id: { $in: req.session.userId } })
+    .exec()
+    .then(user =>
+      res.render("settings", {
         title: "User Settings",
         username: user[0].username,
         userId: req.session.userId
-      });
-    }
-  });
+      })
+    )
+    .catch(error => next(error));
 });
 
-router.post("/", requiresLogin, function(req, res, next) {
-  User.findOneAndUpdate(
-    { _id: req.session.userId },
-    {
-      $set: { avatar: req.body.avatarInput, username: req.body.usernameInput }
-    },
-    function(error, doc) {
-      return res.redirect("/");
-    }
-  );
+router.post("/", requiresLogin, (req, res, next) => {
+  const condition = { _id: req.session.userId };
+  const fieldsToUpdate = {
+    $set: { avatar: req.body.avatarInput, username: req.body.usernameInput }
+  };
+  User.findOneAndUpdate(condition, fieldsToUpdate)
+    .exec()
+    .then(() => res.redirect("/"))
+    .catch(error => next(error));
 });
 
 module.exports = router;
