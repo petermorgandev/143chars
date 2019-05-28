bb9d81f3525905b8886cbd416c0712e9882e6f0d
Petermorgangh <peter@1080peter.com>
2019-05-28 13:20:03 -0600

    Refactor routes/server/profile.js

--- routes/server/profile.js
+++ routes/server/profile.js
@@ -4,9 +4,9 @@ const express = require("express"),
   Message = require("../../models/messages"),
   { requiresLogin } = require("../../middleware");
 
-router.get("/", requiresLogin, (req, res, next) => {
-  return res.redirect("/profile/" + req.session.userId);
-});
+router.get("/", requiresLogin, (req, res, next) =>
+  res.redirect("/profile/" + req.session.userId)
+);
 
 router.get("/:userId", requiresLogin, async function(req, res, next) {
   const user = await User.findOne({ _id: { $in: req.params.userId } });
@@ -14,20 +14,18 @@ router.get("/:userId", requiresLogin, async function(req, res, next) {
   await Message.find({ user: { $in: req.params.userId } })
     .sort({ date: -1 })
     .populate("user", "avatar username")
-    .exec(function(error, messages, username) {
-      if (error) {
-        return next(error);
-      } else {
-        let userData = {
-          title: user.username + "'s Profile",
-          messages: messages,
-          username: user.username,
-          profileId: req.params.userId,
-          userId: req.session.userId
-        };
-        return res.render("profile", userData);
-      }
-    });
+    .exec()
+    .then(messages => {
+      let userData = {
+        title: user.username + "'s Profile",
+        messages: messages,
+        username: user.username,
+        profileId: req.params.userId,
+        userId: req.session.userId
+      };
+      return res.render("profile", userData);
+    })
+    .catch(error => next(error));
 });
 
 module.exports = router;
