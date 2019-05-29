7fef80cd25aa68c8590df63b7419d67ce45a9437
Petermorgangh <peter@1080peter.com>
2019-05-28 23:14:16 -0600

    Set up controller for routes/server/new.js

--- routes/server/new.js
+++ routes/server/new.js
@@ -1,23 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const Message = require("../../models/messages");
 const { requiresLogin } = require("../../middleware");
+const controllers = require('../../controllers/server/new');
 
-router.get("/", requiresLogin, (req, res) => res.render("new", { title: "New Message" }));
-
-router.post("/", requiresLogin, (req, res, next) => {
-  const messageData = {
-    userId: req.session.userId,
-    message: req.body.messageInput,
-    user: req.session.userId
-  };
-
-  Message.create(messageData, function(error, user) {
-    if (error) {
-      return next(error);
-    }
-    return res.redirect("/profile");
-  });
-});
+router.get("/", requiresLogin, controllers.getnewView);
+router.post("/", requiresLogin, controllers.postNew);
 
 module.exports = router;
