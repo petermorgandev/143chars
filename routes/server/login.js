c792181bfa9699875f7fd47c362a95c89bc88be5
Petermorgangh <peter@1080peter.com>
2019-05-28 23:11:36 -0600

    Set up controller for routes/server/login.js

--- routes/server/login.js
+++ routes/server/login.js
@@ -1,27 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/user");
 const middle = require("../../middleware");
+const controllers = require('../../controllers/server/login');
 
-
-router.get("/", middle.loggedOut, (req, res) => res.render("login", { title: "Log In" }));
-
-router.post("/", (req, res, next) => {
-  if (req.body.usernameInput && req.body.passwordInput) {
-    User.authenticate(req.body.usernameInput, req.body.passwordInput, function(error, user) {
-      if (error || !user) {
-        const err = new Error("Wrong username or password.");
-        err.status = 401;
-        return next(err);
-      }
-      req.session.userId = user._id;
-      return res.redirect("/");
-    });
-  } else {
-    const err = new Error("Username and password are required to log in.");
-    err.status = 401;
-    return next(err);
-  }
-});
+router.get("/", middle.loggedOut, controllers.getLoginView);
+router.post("/", controllers.postLogin);
 
 module.exports = router;
