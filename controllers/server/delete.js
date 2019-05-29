9c8afa359f024b8f410776793283bff1405eeab1
Petermorgangh <peter@1080peter.com>
2019-05-28 23:03:50 -0600

    Set up controller for routes/server/delete.js

---
+++ controllers/server/delete.js
@@ -0,0 +1,38 @@
+const User = require("../../models/user");
+const Message = require("../../models/messages");
+
+const deleteSingleMessage = async (req, res, next) => {
+  const getUserId = await Message.findById({ _id: req.params.messageId });
+  
+  const condition = !req.session.userId || getUserId.user != req.session.userId;
+  if (condition) {
+    const err = new Error("You are not authorized to view this page.");
+    err.status = 403;
+    return next(err);
+  }
+
+  await Message.deleteOne({ _id: { $in: req.params.messageId } })
+    .exec()
+    .then(() => res.redirect("/profile"))
+    .catch(error => next(error));
+}
+
+const deleteAllMessages = (req, res, next) => {
+  Message.deleteMany({ user: { $in: req.params.userId } })
+    .exec()
+    .then(() => res.redirect("/profile"))
+    .catch(error => next(error));
+}
+
+const deleteUser = async (req, res, next) => {
+  await Message.deleteMany({ user: { $in: req.session.userId } });
+
+  await User.deleteOne({ _id: req.session.userId })
+    .exec()
+    .then(() => res.redirect("/logout"))
+    .catch(error => next(error));
+}
+
+module.exports.deleteSingleMessage = deleteSingleMessage;
+module.exports.deleteAllMessages = deleteAllMessages;
+module.exports.deleteUser = deleteUser;
--- routes/server/delete.js
+++ routes/server/delete.js
@@ -1,49 +1,22 @@
 const express = require("express");
 const router = express.Router([{ mergeParams: true }]);
-const User = require("../../models/user");
-const Message = require("../../models/messages");
 const { requiresLogin, isCurrentUser } = require("../../middleware");
+const controllers = require('../../controllers/server/delete');
 
-router.get("/message/:messageId", requiresLogin, async (req, res, next) => {
-  const getUserId = await Message.findById({ _id: req.params.messageId });
-  
-  const condition = !req.session.userId || getUserId.user != req.session.userId;
-  if (condition) {
-    const err = new Error("You are not authorized to view this page.");
-    err.status = 403;
-    return next(err);
-  }
-
-  await Message.deleteOne({ _id: { $in: req.params.messageId } })
-    .exec()
-    .then(() => res.redirect("/profile"))
-    .catch(error => next(error));
-});
+router.get("/message/:messageId", requiresLogin, controllers.deleteSingleMessage);
 
 router.get(
   "/messages/:userId",
   requiresLogin,
   isCurrentUser,
-  (req, res, next) => {
-    Message.deleteMany({ user: { $in: req.params.userId } })
-      .exec()
-      .then(() => res.redirect("/profile"))
-      .catch(error => next(error));
-  }
+  controllers.deleteAllMessages
 );
 
 router.get(
   "/user/:userId",
   requiresLogin,
   isCurrentUser,
-  async (req, res, next) => {
-    await Message.deleteMany({ user: { $in: req.session.userId } });
-
-    await User.deleteOne({ _id: req.session.userId })
-      .exec()
-      .then(() => res.redirect("/logout"))
-      .catch(error => next(error));
-  }
+  controllers.deleteUser
 );
 
 module.exports = router;
