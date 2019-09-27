2f6aa01f1f749145b42457cfcee6fb215b803fee
Peter Morgan <peter@1080peter.com>
2019-09-27 09:01:13 -0600

    Cleaned up delete route with destructuring

--- routes/server/delete.js
+++ routes/server/delete.js
@@ -1,22 +1,10 @@
 const express = require("express");
 const router = express.Router([{ mergeParams: true }]);
 const { requiresLogin, isCurrentUser } = require("../../middleware");
-const controllers = require('../../controllers/server/delete');
+const { deleteSingleMessage, deleteAllMessages, deleteUser } = require('../../controllers/server/delete');
 
-router.get("/message/:messageId", requiresLogin, controllers.deleteSingleMessage);
-
-router.get(
-  "/messages/:userId",
-  requiresLogin,
-  isCurrentUser,
-  controllers.deleteAllMessages
-);
-
-router.get(
-  "/user/:userId",
-  requiresLogin,
-  isCurrentUser,
-  controllers.deleteUser
-);
+router.get("/message/:messageId", requiresLogin, deleteSingleMessage);
+router.get("/messages/:userId", requiresLogin, isCurrentUser, deleteAllMessages);
+router.get("/user/:userId", requiresLogin, isCurrentUser, deleteUser);
 
 module.exports = router;
